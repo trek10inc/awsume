d8d2844363b3073ff014caadbfe77f9856d9023e
Charlie <Charlie>
2016-12-14 12:17:30 -0500

    Updated the Windows version of awsume to set AWS_REGION and AWS_DEFAULT_REGION

--- awsume.bat
+++ awsume.bat
@@ -3,7 +3,7 @@
 IF "%1"=="" GOTO displayUsage
 
 IF "%2"=="refresh" (
-  del "%userprofile%\.aws\cli\cache\%1*.json"
+  DEL "%userprofile%\.aws\cli\cache\%1*.json"
 )
 
 call aws s3 ls --profile %1 > nul
@@ -13,23 +13,44 @@ IF ERRORLEVEL 1 GOTO end
 FOR /F %%M in ('DIR %userprofile%\.aws\cli\cache\%1*.json /B /S') DO (
   FOR /F tokens^=16^,20^,28^ delims^=^" %%G IN (%%M) DO (
     IF "%2"=="show" (
-      echo.
-      echo SET AWS_SECRET_ACCESS_KEY=%%G
-      echo SET AWS_SESSION_TOKEN=%%H
-      echo SET AWS_ACCESS_KEY_ID=%%I
+      ECHO.
+      ECHO SET AWS_SECRET_ACCESS_KEY=%%G
+      ECHO SET AWS_SESSION_TOKEN=%%H
+      ECHO SET AWS_ACCESS_KEY_ID=%%I
     )
     SET AWS_SECRET_ACCESS_KEY=%%G
     SET AWS_SESSION_TOKEN=%%H
     SET AWS_ACCESS_KEY_ID=%%I
-    GOTO end
+    GOTO setregion
   ) 
 )
 
+:setregion
+call aws configure get region --profile %1 > awsume_region.txt
+IF ERRORLEVEL 1 GOTO regionerror
+
+SET /p AWS_REGION= < awsume_region.txt
+SET /p AWS_DEFAULT_REGION= < awsume_region.txt
+DEL "./awsume_region.txt"
+
+IF "%2"=="show" (
+  ECHO SET AWS_REGION=%AWS_REGION%
+  ECHO SET AWS_DEFAULT_REGION=%AWS_DEFAULT_REGION%
+)
+GOTO :end
+
+:regionerror
+IF "%2"=="show" (
+  ECHO "# No region set"
+)
+DEL "./awsume_region.txt"
+GOTO :end
+
 
 :displayUsage
-echo.
-echo Missing profile name
-echo. 
-echo Usage: assume profile-name [show]
-echo.
+ECHO.
+ECHO Missing profile name
+ECHO. 
+ECHO Usage: awsume profile-name [show]
+ECHO.
 :end
