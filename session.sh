40a527abeae60d5bb7dd8abba1187dc5e681950d
jhaubold <jhaubold@trek10.com>
2017-01-11 09:35:19 -0500

    update session.sh to prompt for token, to use get-caller-identity

--- session.sh
+++ session.sh
@@ -1,7 +1,14 @@
 #/bin/bash
 
-TOKEN="${1}"
-PROFILE="${2}"
+# usage source session.sh [profilename]
+#
+# you will be prompted for the MFA code
+
+PROFILE="${1}"
+
+# TOKEN="${1}"
+read -s -p "Enter MFA code: " TOKEN
+echo
 
 if [[ -n "$TOKEN" ]] && [[ ${#TOKEN} == 6 ]]; then
 
@@ -9,23 +16,7 @@ if [[ -n "$TOKEN" ]] && [[ ${#TOKEN} == 6 ]]; then
     PROFILE_FLAG="--profile ${PROFILE}"
   fi
 
-  MFA_SERIAL=$(aws iam get-user $PROFILE_FLAG --query User.Arn --output text)
-
-	# If we get an error calling iam get-user $MFA_SERIAL will always be empty
-	if ! [[ -n "$MFA_SERIAL" ]]; then
-
-		# Assuming the error reporting doesn't change greatly this should work nicely
-		MFA_SERIAL=$(aws iam get-user $PROFILE_FLAG --output json 2>&1> /dev/null | sed -n 's/.*\(arn.*\).*/\1/p')
-
-		if ! [[ -n "$MFA_SERIAL" ]]; then
-
-			echo "Failed to obtain MFA serial arn. Make sure to check profile parameter is correct"
-			return 1
-
-		fi
-
-	fi
-
+  MFA_SERIAL=$(aws sts get-caller-identity $PROFILE_FLAG --query Arn --output text)
 
   MFA_SERIAL=${MFA_SERIAL/user/mfa}
 
@@ -40,16 +31,17 @@ if [[ -n "$TOKEN" ]] && [[ ${#TOKEN} == 6 ]]; then
     unset AWS_ACCESS_KEY_ID 2> /dev/null
   fi
   
-  aws sts get-session-token \
-    --output json \
+  CREDENTIALS=$(aws sts get-session-token \
+    --output text \
+    --query Credentials.[AccessKeyId,SecretAccessKey,SessionToken] \
     $PROFILE_FLAG \
-    --duration-seconds 900 \
+    --duration-seconds 3600 \
     --serial-number $MFA_SERIAL \
-    --token-code $TOKEN > ./SESSION
+    --token-code $TOKEN)
 
   if [ "$?" -ne "0" ]; then
 
-    echo "Failed to get session token. Make sure token is first parameter and MFA device is in sync."
+    echo "Failed to get session token. Make sure mfa token is correct and MFA device is in sync and profile exists."
 
     if [[ -n "$PROFILE" ]]; then
       echo "Restoring env credentials"
@@ -58,20 +50,20 @@ if [[ -n "$TOKEN" ]] && [[ ${#TOKEN} == 6 ]]; then
       export AWS_SECRET_ACCESS_KEY=$temp_sak
       export AWS_ACCESS_KEY_ID=$temp_aki
     fi
-    rm ./SESSION 
+    # rm ./SESSION 
     return 1
   fi
-
-  export AWS_SECRET_ACCESS_KEY=$(grep '"SecretAccessKey"' ./SESSION  | cut -d '"' -f4)
-  export AWS_SESSION_TOKEN=$(grep '"SessionToken"' ./SESSION  | cut -d '"' -f4)
-  export AWS_SECURITY_TOKEN=$(grep '"SessionToken"' ./SESSION  | cut -d '"' -f4)
-  export AWS_ACCESS_KEY_ID=$(grep '"AccessKeyId"' ./SESSION  | cut -d '"' -f4)
+  read accessKeyId secretAccessKey sessionToken <<< $(echo $CREDENTIALS)
+  export AWS_ACCESS_KEY_ID=$accessKeyId
+  export AWS_SECRET_ACCESS_KEY=$secretAccessKey
+  export AWS_SESSION_TOKEN=$sessionToken
+  export AWS_SECURITY_TOKEN=$sessionToken
   printf " ---\nSession successfully set for profile/user: "$PROFILE"\n ---\n"
-  rm ./SESSION
+  # rm ./SESSION
 
 else
 
-    echo "Please pass a MFA token as parameters and a configured aws cli profile name, in that order"
+    echo "MFA token invalid"
 
 fi
 
