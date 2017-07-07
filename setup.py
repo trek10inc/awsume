d836a4ebf234d14ab77974867307991e8704d29b
Michael R. Barney, Jr <mbarneyjr5309@gmail.com>
2017-07-07 16:03:06 -0400

    Updates Profile

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 from setuptools.command.install_scripts import install_scripts
 
-version = '1.1.5'
+version = '1.1.6'
 
 class CustomInstall(install):
     def run(self):
