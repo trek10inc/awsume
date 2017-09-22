a038807238351619d8743dcfd32abc11757d9a8f
Michael Barney, Jr <mbarneyjr5309@gmail.com>
2017-09-22 14:42:13 -0400

    Adds auto-complete scripts for bash and zsh

--- setup.py
+++ setup.py
@@ -6,55 +6,111 @@ from setuptools.command.install_scripts import install_scripts
 version = '1.2.6'
 
 class CustomInstall(install):
+    #the auto-complete scripts
+    bashScript = """#auto-completion script for Awsume
+_awsume() {
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    opts=$(awsumepy --rolesusers)
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+}
+complete -F _awsume awsume"""
+
+    zshScript = """#compdef awsume
+_arguments "*: :($(awsumepy --rolesusers))" """
+
+    def use_bash(self, homefolder):
+        #the possible bash rc files
+        bashFiles = ['%s/.bash_aliases', '%s/.bashrc', '%s/.bash_profile', '%s/.profile', '%s/.login']
+        return any([os.path.exists(os.path.abspath(f % homefolder)) for f in bashFiles])
+
+    def get_bash_file(self, homefolder):
+        if os.path.exists(os.path.abspath('%s/.bash_aliases' % homefolder)):
+            rc_file = os.path.abspath('%s/.bash_aliases' % homefolder)
+        elif os.path.exists(os.path.abspath('%s/.bashrc' % homefolder)):
+            rc_file = os.path.abspath('%s/.bashrc' % homefolder)
+        elif os.path.exists(os.path.abspath('%s/.bash_profile' % homefolder)):
+            rc_file = os.path.abspath('%s/.bash_profile' % homefolder)
+        elif os.path.exists(os.path.abspath('%s/.profile' % homefolder)):
+            rc_file = os.path.abspath('%s/.profile' % homefolder)
+        elif os.path.exists(os.path.abspath('%s/.login' % homefolder)):
+            rc_file = os.path.abspath('%s/.login' % homefolder)
+        return rc_file
+
+    def use_zsh(self, homefolder):
+        #the possible bash rc files
+        zshFiles = ['%s/.zshrc', '%s/.zshenv', '%s/.zprofile', '%s/.zlogin']
+        return any([os.path.exists(os.path.abspath(f % homefolder)) for f in zshFiles])
+
+    def get_zsh_file(self, homefolder):
+        if os.path.exists(os.path.abspath('%s/.zshrc' % homefolder)):
+            rc_file = os.path.abspath('%s/.zshrc' % homefolder)
+        elif os.path.exists(os.path.abspath('%s/.zshenv' % homefolder)):
+            rc_file = os.path.abspath('%s/.zshenv' % homefolder)
+        elif os.path.exists(os.path.abspath('%s/.zprofile' % homefolder)):
+            rc_file = os.path.abspath('%s/.zprofile' % homefolder)
+        elif os.path.exists(os.path.abspath('%s/.zlogin' % homefolder)):
+            rc_file = os.path.abspath('%s/.zlogin' % homefolder)
+        return rc_file
+
+    def add_alias(self, rc_file, alias):
+        with open(rc_file, 'r') as f:
+            lines = f.readlines()
+            if alias not in lines:
+                out = open(rc_file, 'a')
+                out.write("#AWSume alias to source the AWSume script\n")
+                out.write(alias)
+                out.close()
+
+    def add_bash_script(self, rc_file, bashScript):
+        with open(rc_file, 'r') as f:
+            content = f.read()
+            if not bashScript in content:
+                out = open(rc_file, 'a')
+                out.write('\n#Auto-Complete function for AWSume')
+                out.write('\n' + bashScript + '\n')
+                out.close()
+
+    def add_zsh_script(self, zshScript, rc_file, func_dir):
+        #if for some reason /usr/local/share/zsh/site-functions doesn't exist, add it
+        if not os.path.exists(func_dir):
+            os.makedirs(func_dir)
+            #add the directory to fpath
+            with open(rc_file, 'r') as original: data = original.read()
+            with open(rc_file, 'w') as modified: modified.write('fpath=$(' + func_dir + ' $fpath)\n' + data)
+        #add _awsume completion function to site-functions directory
+        func_file = func_dir + '/_awsume'
+        with open(func_file, 'w+') as f:
+            content = f.read()
+            if not zshScript in content:
+                out = open(func_file, 'a')
+                out.write(zshScript + '\n')
+                out.close()
+
     def run(self):
         def _post_install():
             #cross platform home folder
             homefolder = os.path.expanduser('~')
+
             #alias string to add
             alias = 'alias awsume=". awsume"\n'
-            rc_file = ""
 
-            #the possible bash rc files
-            if os.path.exists(os.path.abspath('%s/.bash_aliases' % homefolder)):
-                rc_file = os.path.abspath('%s/.bash_aliases' % homefolder)
-            elif os.path.exists(os.path.abspath('%s/.bashrc' % homefolder)):
-                rc_file = os.path.abspath('%s/.bashrc' % homefolder)
-            elif os.path.exists(os.path.abspath('%s/.bash_profile' % homefolder)):
-                rc_file = os.path.abspath('%s/.bash_profile' % homefolder)
-            elif os.path.exists(os.path.abspath('%s/.profile' % homefolder)):
-                rc_file = os.path.abspath('%s/.profile' % homefolder)
-            elif os.path.exists(os.path.abspath('%s/.login' % homefolder)):
-                rc_file = os.path.abspath('%s/.login' % homefolder)
+            #add bash alias/script
+            if self.use_bash(homefolder):
+                rc_file = self.get_bash_file(homefolder)
+                self.add_alias(rc_file, alias)
+                self.add_bash_script(rc_file, self.bashScript)
 
-            #now add the alias to the user's rc file
-            if os.path.exists(rc_file):
-                with open(rc_file, 'r') as f:
-                    lines = f.readlines()
-                    if alias not in lines:
-                        out = open(rc_file, 'a')
-                        out.write("#AWSume alias to source the AWSume script\n")
-                        out.write(alias)
-                        out.close()
+            #add zsh alias/script
+            if self.use_zsh(homefolder):
+                rc_file = self.get_zsh_file(homefolder)
+                fpath_dir = os.path.abspath('/usr/local/share/zsh/site-functions')
+                self.add_alias(rc_file, alias)
+                self.add_zsh_script(self.zshScript, rc_file, fpath_dir)
 
-            #the possible zsh rc files
-            if os.path.exists(os.path.abspath('%s/.zshrc' % homefolder)):
-                rc_file = os.path.abspath('%s/.zshrc' % homefolder)
-            elif os.path.exists(os.path.abspath('%s/.zshenv' % homefolder)):
-                rc_file = os.path.abspath('%s/.zshenv' % homefolder)
-            elif os.path.exists(os.path.abspath('%s/.zprofile' % homefolder)):
-                rc_file = os.path.abspath('%s/.zprofile' % homefolder)
-            elif os.path.exists(os.path.abspath('%s/.zlogin' % homefolder)):
-                rc_file = os.path.abspath('%s/.zlogin' % homefolder)
-
-            #now add the alias to the user's rc file
-            if os.path.exists(rc_file):
-                with open(rc_file, 'r') as f:
-                    lines = f.readlines()
-                    if alias not in lines:
-                        out = open(rc_file, 'a')
-                        out.write("#AWSume alias to source the AWSume script\n")
-                        out.write(alias)
-                        out.close()
         atexit.register(_post_install)
         install.run(self)
 
@@ -86,7 +142,6 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        'awscli',
         'python-dateutil',
         'boto3',
         'psutil',
