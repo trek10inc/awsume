fff09c0e254cfc1d8e6cf050f06b9736c8137856
Michael Barney, Jr <mbarneyjr5309@gmail.com>
2017-12-08 12:15:53 -0500

    updates version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools.command.install import install
 from setuptools.command.install_scripts import install_scripts
 from distutils.spawn import find_executable
 
-version = '1.3.2'
+version = '2.0.0'
 
 class CustomInstall(install):
 
