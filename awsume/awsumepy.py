5266cfeafb0d44fecf211c09b983a92aa854f34c
Michael Barney, Jr <mbarneyjr5309@gmail.com>
2017-12-08 13:10:25 -0500

    Fix profile name bug

--- awsume/awsumepy.py
+++ awsume/awsumepy.py
@@ -5,7 +5,7 @@ from six.moves import configparser as ConfigParser
 from builtins import input
 from yapsy import PluginManager
 
-__version__ = '2.1.0'
+__version__ = '2.1.1'
 
 #initialize logging
 logging.getLogger('yapsy').addHandler(logging.StreamHandler())
