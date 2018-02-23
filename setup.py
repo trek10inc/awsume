3e440e4c8571de341feaac84327128b9f45ab285
Michael Barney, Jr <mbarneyjr5309@gmail.com>
2018-02-23 16:33:05 -0500

    Reworks install scripts, reads version from package.json

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-import atexit, os, subprocess
-import urllib
-from setuptools import setup, find_packages
+import atexit
+import os
+import subprocess
+import json
+from distutils.spawn import find_executable
+from setuptools import setup
 from setuptools.command.install import install
 from setuptools.command.install_scripts import install_scripts
-from distutils.spawn import find_executable
 
-version = '2.1.5'
+PACKAGE = json.load(open('package.json'))
+HOME_FOLDER = os.path.expanduser('~')
 
-class CustomInstall(install):
-
-    #the auto-complete scripts
-    bashScript = """_awsume() {
+BASH_AUTOCOMPLETE_SCRIPT = """
+_awsume() {
     local cur prev opts
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
@@ -19,12 +20,14 @@ class CustomInstall(install):
     COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
     return 0
 }
-complete -F _awsume awsume"""
-
-    zshScript = """#compdef awsume
-_arguments "*: :($(awsumepy --rolesusers))" """
-
-    powershellScript = """Register-ArgumentCompleter -Native -CommandName awsume -ScriptBlock {
+complete -F _awsume awsume
+"""
+ZSH_AUTOCOMPLETE_SCRIPT = """
+#compdef awsume
+_arguments "*: :($(awsumepy --rolesusers))"
+"""
+POWERSHELL_AUTOCOMPLETE_SCRIPT = """
+Register-ArgumentCompleter -Native -CommandName awsume -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
     $(awsumepy --rolesusers) |
     Where-Object { $_ -like "$wordToComplete*" } |
@@ -32,113 +35,143 @@ _arguments "*: :($(awsumepy --rolesusers))" """
     ForEach-Object {
         [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
-}"""
+}
+"""
+
+class CustomInstall(install):
+    """Run the post-install scripts to load in autocomplete, alias, and anything else for awsume."""
+    def get_awsume_alias(self):
+        """Return the alias for awsume.
+        If pyenv is being used, return an alias to the current python installation's awsume."""
+        awsume_alias = 'alias awsume=". awsume"'
+        if find_executable('pyenv'):
+            pyenv_version = os.popen('pyenv version').read().split('(')[0].strip()
+            awsume_alias = 'alias awsume=". ~/.pyenv/versions/' + pyenv_version + '/bin/awsume"'
+        return awsume_alias
+
+    def install_alias(self, file_path, alias):
+        """Install the given aliad to the given file.
+        Add a comment above the alias so that users know what it's for."""
+        with open(file_path, 'a+') as read_f:
+            lines = read_f.readlines()
+            if alias not in lines:
+                out = open(file_path, 'a')
+                out.write('\n')
+                out.write('#AWSume alias to source the AWSume script')
+                out.write('\n')
+                out.write(alias)
+                out.write('\n')
+                out.close()
+
+    def install_bash_script(self, file_path, script):
+        """Install AWSume's auto-complete to bash rc file."""
+        with open(file_path, 'a+') as read_f:
+            content = read_f.read()
+            if not script in content:
+                out = open(file_path, 'a')
+                out.write('\n')
+                out.write('#Auto-Complete function for AWSume')
+                out.write('\n')
+                out.write(script)
+                out.write('\n')
+                out.close()
+
+    def install_zsh_script(self, zsh_script, rc_file, function_path):
+        """Install AWSume's auto-complete to zsh rc file."""
+        if not os.path.exists(function_path):
+            os.makedirs(function_path)
+            #add the directory to fpath
+            with open(rc_file, 'r') as original:
+                data = original.read()
+            with open(rc_file, 'w') as modified:
+                modified.write('fpath=$(' + function_path + ' $fpath)\n' + data)
+
+        func_file = function_path + '/_awsume'
+        with open(func_file, 'w+') as read_f:
+            content = read_f.read()
+            if not zsh_script in content:
+                out = open(func_file, 'a')
+                out.write(zsh_script + '\n')
+                out.close()
+
+    def install_powershell_script(self, script, powershell_file):
+        """Install AWSume's auto-complete to powershell profile."""
+        contents = open(powershell_file, 'w+').read()
+        if script not in contents:
+            out = open(powershell_file, 'a+')
+            out.write('\n')
+            out.write(script)
+            out.write('\n')
+            out.close()
 
     def get_bash_file(self, homefolder):
-        # default rc_file
-        rc_file = os.path.abspath('%s/.profile' % homefolder)
+        """Return the path to the user's bash rc file."""
+        rc_file = os.path.abspath('%s/.bashrc' % homefolder)
         if os.path.exists(os.path.abspath('%s/.bash_aliases' % homefolder)):
             rc_file = os.path.abspath('%s/.bash_aliases' % homefolder)
         elif os.path.exists(os.path.abspath('%s/.bashrc' % homefolder)):
             rc_file = os.path.abspath('%s/.bashrc' % homefolder)
         elif os.path.exists(os.path.abspath('%s/.bash_profile' % homefolder)):
             rc_file = os.path.abspath('%s/.bash_profile' % homefolder)
+        elif os.path.exists(os.path.abspath('%s/.profile' % homefolder)):
+            rc_file = os.path.abspath('%s/.profile' % homefolder)
         elif os.path.exists(os.path.abspath('%s/.login' % homefolder)):
             rc_file = os.path.abspath('%s/.login' % homefolder)
         return rc_file
 
     def get_zsh_file(self, homefolder):
-        # default
-        rc_file = os.path.abspath('%s/.zprofile' % homefolder)
+        """Return the path to the user's zsh rc file."""
+        rc_file = os.path.abspath('%s/.zshrc' % homefolder)
         if os.path.exists(os.path.abspath('%s/.zshrc' % homefolder)):
             rc_file = os.path.abspath('%s/.zshrc' % homefolder)
         elif os.path.exists(os.path.abspath('%s/.zshenv' % homefolder)):
             rc_file = os.path.abspath('%s/.zshenv' % homefolder)
+        elif os.path.exists(os.path.abspath('%s/.zprofile' % homefolder)):
+            rc_file = os.path.abspath('%s/.zprofile' % homefolder)
         elif os.path.exists(os.path.abspath('%s/.zlogin' % homefolder)):
             rc_file = os.path.abspath('%s/.zlogin' % homefolder)
         return rc_file
 
-    def add_alias(self, rc_file, alias):
-        with open(rc_file, 'a+') as read_f:
-            lines = read_f.readlines()
-            if alias not in lines:
-                out = open(rc_file, 'a')
-                out.write("\n#AWSume alias to source the AWSume script\n")
-                out.write(alias)
-                out.close()
-
-    def add_bash_script(self, rc_file, bashScript):
-        with open(rc_file, 'a+') as read_f:
-            content = read_f.read()
-            if not bashScript in content:
-                out = open(rc_file, 'a')
-                out.write('\n#Auto-Complete function for AWSume')
-                out.write('\n' + bashScript + '\n')
-                out.close()
-
-    def add_zsh_script(self, zsh_script, rc_file, func_dir):
-        #if /usr/local/share/zsh/site-functions doesn't exist, add it
-        if not os.path.exists(func_dir):
-            os.makedirs(func_dir)
-            #add the directory to fpath
-            with open(rc_file, 'r') as original: data = original.read()
-            with open(rc_file, 'w') as modified: modified.write('fpath=$(' + func_dir + ' $fpath)\n' + data)
-
-        #add _awsume completion function to site-functions directory
-        func_file = func_dir + '/_awsume'
-        with open(func_file, 'w+') as f:
-            content = f.read()
-            if not zsh_script in content:
-                out = open(func_file, 'a')
-                out.write(zsh_script + '\n')
-                out.close()
-
-    def add_powershell_script(self, script, powershell_file):
-        contents = open(powershell_file, 'w+').read()
-        if script not in contents:
-            f = open(powershell_file, 'a+')
-            f.write('\n' + script + '\n')
-            f.close()
+    def ensure_executable(self):
+        """Make sure AWSume is executable"""
+        (awsume_path, _) = subprocess.Popen(["which", "awsume"], stdout=subprocess.PIPE).communicate()
+        awsume_path = awsume_path.strip()
+        if os.path.exists(awsume_path):
+            os.chmod(awsume_path, int('755', 8))
 
     def run(self):
         def _post_install():
-            #cross platform home folder
-            homefolder = os.path.expanduser('~')
+            """Run post-install operations"""
+            awsume_alias = self.get_awsume_alias()
 
-            #alias string to add
-            alias = 'alias awsume=". awsume"\n'
+            # install to bash
+            bash_rc_file = self.get_bash_file(HOME_FOLDER)
+            self.install_alias(bash_rc_file, awsume_alias)
+            self.install_bash_script(bash_rc_file, BASH_AUTOCOMPLETE_SCRIPT)
 
-            #pyenv compatibility
-            if find_executable('pyenv'):
-                pyenv_version = os.popen('pyenv version').read().split('(')[0].strip()
-                alias = 'alias awsume=". ~/.pyenv/versions/' + pyenv_version + '/bin/awsume"'
-
-            # add bash alias/script, add alias to bash file
-            # regardless of platform for compatibility
-            rc_file = self.get_bash_file(homefolder)
-            self.add_alias(rc_file, alias)
-            self.add_bash_script(rc_file, self.bashScript)
-
-            #add zsh alias/script
+            # install to zsh
             if find_executable('zsh'):
-                rc_file = self.get_zsh_file(homefolder)
-                fpath_dir = os.path.abspath('/usr/local/share/zsh/site-functions')
-                self.add_alias(rc_file, alias)
-                self.add_zsh_script(self.zshScript, rc_file, fpath_dir)
+                zsh_rc_file = self.get_zsh_file(HOME_FOLDER)
+                function_path = os.path.abspath('/usr/local/share/zsh/site-functions')
+                self.install_alias(zsh_rc_file, awsume_alias)
+                self.install_zsh_script(ZSH_AUTOCOMPLETE_SCRIPT, zsh_rc_file, function_path)
 
-            #add powershell script
+            # install to powershell
             if find_executable('powershell'):
-                (file_name, err) = subprocess.Popen(["powershell", "$profile"], stdout=subprocess.PIPE, shell=True).communicate()
+                (file_name, _) = subprocess.Popen(["powershell", "$profile"], stdout=subprocess.PIPE, shell=True).communicate()
                 file_name = str(file_name.decode('ascii')).replace('\r\n', '')
-                self.add_powershell_script(self.powershellScript, file_name)
+                self.install_powershell_script(POWERSHELL_AUTOCOMPLETE_SCRIPT, file_name)
 
+            # make executable
+            self.ensure_executable()
         atexit.register(_post_install)
         install.run(self)
 
 class CustomInstallScripts(install_scripts):
+    """Ensure AWSume is executable"""
     def run(self):
         def _post_install_scripts():
-            #make sure awsume is executable
+            """Ensure AWSume is executable"""
             execpath = '/usr/local/bin/awsume'
             if os.path.exists(execpath):
                 os.chmod(execpath, int('755', 8))
@@ -146,22 +179,14 @@ class CustomInstallScripts(install_scripts):
         install_scripts.run(self)
 
 setup(
-    name="awsume",
-    packages=find_packages(exclude=("*test*", "./awsume/test*", "./awsume/testAwsume.py")),
-    version=version,
-    author="Trek10, Inc",
-    author_email="package-management@trek10.com",
-    description="Utility for easily assuming AWS IAM roles from the command line, now in Python!",
-    long_description=open('README.rst').read(),
-    license="MIT",
-    url='https://github.com/trek10inc/awsume',
-    download_url='https://github.com/trek10inc/awsume/archive/' + version + '.tar.gz',
-    scripts=[
-        'awsume/shellScripts/awsume',
-        'awsume/shellScripts/awsume.ps1',
-        'awsume/shellScripts/awsume.bat',
-    ],
-    include_package_data=True,
+    name=PACKAGE['name'],
+    packages=['awsume'],
+    version=PACKAGE['version'],
+    author=PACKAGE['author']['name'],
+    author_email=PACKAGE['author']['email'],
+    description=PACKAGE['description'],
+    license=PACKAGE['license'],
+    url=PACKAGE['homepage'],
     install_requires=[
         'python-dateutil',
         'boto3',
@@ -169,10 +194,15 @@ setup(
         'yapsy',
         'future'
     ],
+    scripts=[
+        'awsume/shellScripts/awsume',
+        'awsume/shellScripts/awsume.ps1',
+        'awsume/shellScripts/awsume.bat',
+    ],
     entry_points={
         "console_scripts": [
             'awsumepy=awsume.awsumepy:main',
-            'autoAwsume=awsume.autoAwsume:main'
+            'autoAwsume=awsume.autoAwsume:main',
         ]
     },
     cmdclass={
