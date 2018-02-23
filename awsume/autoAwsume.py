52da6afe814eab6fdaf2c0847bd6751350115ea6
Michael Barney, Jr <mbarneyjr5309@gmail.com>
2018-02-23 16:34:02 -0500

    Refactors autoAwsume to be more testable

--- awsume/autoAwsume.py
+++ awsume/autoAwsume.py
@@ -1,84 +1,118 @@
+"""autoAwsume - A daemon to auto refresh 'auto-refresh' profiles in the credentials file"""
 from __future__ import print_function
 import sys
 import datetime
 import time
+import botocore
+import dateutil
+
 from awsume import awsumepy
 from awsume.awsumepy import AWS_CACHE_DIRECTORY, AWS_CREDENTIALS_FILE
 
-def refresh_session(oldSession, roleArn, sessionName):
-    """
-    oldSession - the session to refresh;
-    roleArn - the role_arn used to make the assume_role call;
-    sessionName - what to name the assumed role session;
-    refresh the `oldSession` role credentials and return them
-    """
-    #create the client to make aws calls
-    refreshClient = awsumepy.create_boto_sts_client(None,
-                                                    oldSession['SecretAccessKey'],
-                                                    oldSession['AccessKeyId'],
-                                                    oldSession['SessionToken'])
-    #call assume_role
-    roleCredentials = awsumepy.get_assume_role_credentials(refreshClient,
-                                                           roleArn,
-                                                           sessionName)
-    #format the credentials for awsume
-    newRoleSession = awsumepy.create_awsume_session(roleCredentials, oldSession)
-    #localize the expiration
-    newRoleSession['Expiration'] = newRoleSession['Expiration'].replace(tzinfo=None)
-    return newRoleSession
+def get_now(): # pragma: no cover
+    """Return datetime.datetime.now()."""
+    return datetime.datetime.now()
 
-def scan_through_auto_refresh_profiles(credentialsProfiles):
+def refresh_session(autoProfile):
+    """Refresh the `oldSession` role credentials.
+
+    Parameters
+    ----------
+    - oldSession - the session to refresh;
+    - roleArn - the role_arn used to make the assume_role call;
+    - sessionName - what to name the assumed role session;
+
+    Returns
+    -------
+    The refreshed role session
     """
-    credentialsProfiles - the dict of profiles to scan through;
-    loop through the `credentialsProfiles`, find any that are 'auto-refresh-' profiles,
-    refresh/remove any expired ones, and return when the earliest session-expiration will happen
+    sourceCredentials = awsumepy.read_aws_cache(AWS_CACHE_DIRECTORY, autoProfile['awsume_cache_name'])
+    stsClient = awsumepy.create_sts_client(sourceCredentials['AccessKeyId'],
+                                           sourceCredentials['SecretAccessKey'],
+                                           sourceCredentials['SessionToken'])
+    try:
+        response = stsClient.assume_role(RoleArn=autoProfile['aws_role_arn'], RoleSessionName=autoProfile['awsume_session_name'])
+        session = response['Credentials']
+        session['Expiration'] = session['Expiration'].astimezone(dateutil.tz.tzlocal())
+        session['Expiration'] = session['Expiration'].strftime('%Y-%m-%d %H:%M:%S')
+        session['region'] = sourceCredentials['region']
+
+        autoProfile['aws_access_key_id'] = session['AccessKeyId']
+        autoProfile['aws_secret_access_key'] = session['SecretAccessKey']
+        autoProfile['aws_session_token'] = session['SessionToken']
+        autoProfile['awsume_role_expiration'] = session['Expiration']
+        awsumepy.write_auto_awsume_session(autoProfile['__name__'].replace('auto-refresh-', ''), autoProfile, AWS_CREDENTIALS_FILE)
+    except botocore.exceptions.ClientError:
+        pass
+
+def extract_auto_refresh_profiles(profiles):
+    """Pull out any profiles with the prefix 'auto-refresh-' in the name.
+
+    Parameters
+    ----------
+    - profiles - the profiles read from the aws credentials file
+
+    Returns
+    -------
+    A dict of profiles that are prefixed by 'auto-refresh-' in the name.
     """
-    for profile in credentialsProfiles:
-        expirationList = []
-        #if we're looking at an auto-refreshed profile
+    autoRefreshProfiles = {}
+    for profile in profiles:
         if 'auto-refresh-' in profile:
-            #get the cache filename (the file that contains source_profile credentials)
-            cacheFileName = credentialsProfiles[profile]['awsume_cache_file']
-            #get the source profile's credentials
-            sourceProfileCredentials = awsumepy.read_awsume_session_from_file(AWS_CACHE_DIRECTORY, cacheFileName)
+            autoRefreshProfiles[profile] = profiles[profile]
+    return autoRefreshProfiles
 
-            #if credentials are not expired
-            if sourceProfileCredentials['Expiration'] > datetime.datetime.now():
-                try:
-                    #refresh the session
-                    refreshedCredentials = refresh_session(sourceProfileCredentials, credentialsProfiles[profile]['aws_role_arn'], profile.replace('auto-awsume-', '') + '-auto-awsume-session')
-                except Exception as e:
-                    #if refreshing the session failed, remove that profile
-                    print("autoAwsume: Refreshing profile [" + profile.replace('auto-refresh-', '') + "] failed. That profile will no longer be auto-refreshed.")
-                    print(str(e))
-                    awsumepy.remove_auto_awsume_profile_by_name(profile.replace('auto-refresh-',''), AWS_CREDENTIALS_FILE)
-                else:
-                    #write the session
-                    awsumepy.write_auto_awsume_session(profile, refreshedCredentials, cacheFileName, credentialsProfiles[profile]['aws_role_arn'], AWS_CREDENTIALS_FILE)
-                    expirationList.append(min(sourceProfileCredentials['Expiration'], refreshedCredentials['Expiration']))
-            #if credentials are expired
-            else:
-                awsumepy.remove_auto_awsume_profile_by_name(profile.replace('auto-refresh-',''), AWS_CREDENTIALS_FILE)
-    if expirationList:
-        return min(expirationList)
+def get_earliest_expiration(autoProfiles):
+    """Get the earliest expiration from the autoProfiles
+
+    Parameters
+    ----------
+    - autoProfiles - the autoAwsume profiles from the credentials profile
+
+    Returns
+    -------
+    A datetime object containing the earliest expiration.
+    """
+    expirations = []
+    for profile in autoProfiles:
+        expirations.append(
+            datetime.datetime.strptime(
+                autoProfiles[profile]['awsume_user_expiration'], '%Y-%m-%d %H:%M:%S'))
+        expirations.append(
+            datetime.datetime.strptime(
+                autoProfiles[profile]['awsume_role_expiration'], '%Y-%m-%d %H:%M:%S'))
+    if expirations:
+        return min(expirations)
     else:
-        return datetime.datetime.now()
+        return get_now()
+
+def refresh_expired_profiles(autoProfiles):
+    """Refresh any expired autoProfiles.
+
+    Parameters
+    ----------
+    - autoProfiles - the autoAwsume profiles from the credentials profile
+    """
+    for profile in autoProfiles:
+        userExpiration = datetime.datetime.strptime(autoProfiles[profile]['awsume_user_expiration'], '%Y-%m-%d %H:%M:%S')
+        roleExpiration = datetime.datetime.strptime(autoProfiles[profile]['awsume_role_expiration'], '%Y-%m-%d %H:%M:%S')
+        if roleExpiration < get_now():
+            refresh_session(autoProfiles[profile])
+        if userExpiration < get_now():
+            awsumepy.remove_auto_profile(autoProfiles[profile]['__name__'].replace('auto-refresh-', ''))
 
 def main():
     while True:
-        #get the list of profiles
-        autoAwsumeProfiles = awsumepy.get_profiles_from_ini_file(AWS_CREDENTIALS_FILE)
-        #look for the earliest expiration and if possible, refresh any expired sessions
-        earliestExpiration = scan_through_auto_refresh_profiles(autoAwsumeProfiles)
-        #calculate the time until the earliest expiration
-        timeUntilEarliestExpiration = (earliestExpiration - datetime.datetime.now().replace(tzinfo=earliestExpiration.tzinfo)).total_seconds()
-        #if that time has already expired
+        credentialsProfiles = awsumepy.read_ini_file(AWS_CREDENTIALS_FILE)
+        autoRefreshProfiles = extract_auto_refresh_profiles(credentialsProfiles)
+        refresh_expired_profiles(autoRefreshProfiles)
+        earliestExpiration = get_earliest_expiration(autoRefreshProfiles)
+        timeUntilEarliestExpiration = (earliestExpiration - get_now().replace(tzinfo=earliestExpiration.tzinfo)).total_seconds()
         if timeUntilEarliestExpiration <= 0:
             break
-        #wait until the next session expires to run again
+        print("autoAwsume: Sleeping for " + str(timeUntilEarliestExpiration) + " seconds", file=sys.stderr)
         time.sleep(timeUntilEarliestExpiration)
+    print("autoAwsume: No more credentials left to refresh, shutting down", file=sys.stderr)
 
-    print("#autoAwsume: No more credentials left to refresh, shutting down", file=sys.stderr)
-
-if __name__ == '__main__':
+if __name__ == '__main__': # pragma: no cover
     main()
