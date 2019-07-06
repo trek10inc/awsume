d79ef077c4f2b44720ab282f29d390890f19954f
Michael Barney, Jr <mbarneyme@gmail.com>
2019-07-06 17:39:22 -0400

    adds install requires, fixes some imports

--- awsume/autoawsume/__init__.py
+++ awsume/autoawsume/__init__.py
@@ -1,1 +1,2 @@
-from . import *
+from . import main
+from . import process
--- awsume/awsumepy/__init__.py
+++ awsume/awsumepy/__init__.py
@@ -1,3 +1,7 @@
-from . import *
+from . import app
+from . import default_plugins
+from . import hookspec
+from . import main
+from . import lib
 from . hookimpl import hookimpl
 from . lib import safe_print
