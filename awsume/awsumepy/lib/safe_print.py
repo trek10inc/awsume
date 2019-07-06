1fce6d844f4c0d2464341c1c7abc77952dacebe0
Michael Barney, Jr <mbarneyme@gmail.com>
2019-07-06 18:51:06 -0400

    Fixes colors for windows

--- awsume/awsumepy/lib/safe_print.py
+++ awsume/awsumepy/lib/safe_print.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import json
 import colorama
@@ -7,6 +8,6 @@ from .constants import AWSUME_CONFIG
 def safe_print(message: str, color: str = '', end: str = None):
     """Safely print so no data is interfering with the shell wrapper"""
     config = json.load(open(str(AWSUME_CONFIG), 'r'))
-    if config.get('colors') != 'true':
+    if os.name == 'nt' or config.get('colors') != 'true':
         color = ''
     print(str(color) + str(message) + colorama.Style.RESET_ALL, end=end, file=sys.stderr)
