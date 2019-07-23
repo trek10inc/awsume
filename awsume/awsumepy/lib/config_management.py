b36040c7206404488052836576fd80e489613549
Michael Barney, Jr <mbarneyme@gmail.com>
2019-07-22 20:00:35 -0400

    uses yaml for config management

--- awsume/awsumepy/lib/config_management.py
+++ awsume/awsumepy/lib/config_management.py
@@ -1,10 +1,13 @@
-import os
 import json
+import os
+
 import colorama
 
+import yaml
+
 from . import constants
-from . logger import logger
-from . safe_print import safe_print
+from .logger import logger
+from .safe_print import safe_print
 
 
 defaults = {
@@ -19,11 +22,15 @@ def load_config() -> dict:
     if not os.path.isfile(str(constants.AWSUME_CONFIG)):
         open(str(constants.AWSUME_CONFIG), 'a').close()
 
-    options = {}
+    options = None
     try:
-        options = json.load(open(str(constants.AWSUME_CONFIG), 'r'))
-    except json.JSONDecodeError:
-        write_config(defaults)
+        options = yaml.safe_load(open(str(constants.AWSUME_CONFIG), 'r'))
+    except Exception:
+        safe_print('Cannot parse config file: {}'.format(constants.AWSUME_CONFIG), colorama.Fore.RED)
+        exit(1)
+        # write_config(defaults)
+        # options = defaults
+    if not options:
         options = defaults
     return options
 
@@ -35,7 +42,7 @@ def write_config(config: dict):
         open(str(constants.AWSUME_CONFIG), 'a').close()
 
     try:
-        json.dump(config, open(str(constants.AWSUME_CONFIG), 'w'), indent=2)
+        yaml.safe_dump(config, open(str(constants.AWSUME_CONFIG), 'w'))
     except Exception as e:
         safe_print('Unable to write config: {}'.format(e), colorama.Fore.RED)
 
@@ -45,11 +52,19 @@ def update_config(operations: list):
     config = load_config()
     if operations[0].lower() == 'set':
         logger.debug('Setting {} to {}'.format(operations[1], operations[2]))
-        config[operations[1]] = operations[2]
+        try:
+            value = json.loads(operations[2])
+        except json.JSONDecodeError:
+            logger.debug('Cannot parse input', exc_info=True)
+            value = operations[2]
+        config[operations[1]] = value
     if operations[0].lower() in ['reset', 'clear']:
         if operations[1] in defaults:
             config[operations[1]] = defaults[operations[1]]
             safe_print('Reset {} to {}'.format(operations[1], defaults[operations[1]]), colorama.Fore.YELLOW)
+        elif operations[1] in config:
+            del config[operations[1]]
+            safe_print('Deleted key {}'.format(operations[1]), colorama.Fore.YELLOW)
         else:
             safe_print('Key not a valid default: {}'.format(operations[1]), colorama.Fore.YELLOW)
     write_config(config)
--- awsume/awsumepy/lib/constants.py
+++ awsume/awsumepy/lib/constants.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 AWSUME_DIR = Path('~/.awsume').expanduser()
-AWSUME_CONFIG = Path('~/.awsume' + '/config.json').expanduser()
+AWSUME_CONFIG = Path('~/.awsume' + '/config.yaml').expanduser()
 
 DEFAULT_CREDENTIALS_FILE = Path('~/.aws/credentials').expanduser()
 DEFAULT_CONFIG_FILE = Path('~/.aws/config').expanduser()
