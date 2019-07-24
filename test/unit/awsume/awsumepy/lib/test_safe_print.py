50b2d1f6e3493c3ba265a15560977d150587fc5f
Michael Barney, Jr <mbarneyme@gmail.com>
2019-07-24 19:22:55 -0400

    fixes tests on windows

--- test/unit/awsume/awsumepy/lib/test_safe_print.py
+++ test/unit/awsume/awsumepy/lib/test_safe_print.py
@@ -6,19 +6,28 @@ from unittest.mock import patch, MagicMock
 from awsume.awsumepy.lib.safe_print import safe_print
 
 
+@patch('yaml.safe_load')
 @patch('sys.stderr', new_callable=StringIO)
 @patch('sys.stdout', new_callable=StringIO)
 @patch.object(safe_print, 'open', create=True)
-def test_safe_print(open: MagicMock, stdout: MagicMock, stderr: MagicMock):
-    safe_print('Text')
+def test_safe_print(open: MagicMock, stdout: MagicMock, stderr: MagicMock, yaml_load: MagicMock):
+    yaml_load.return_value = {'colors': True}
+    with patch.object(os, 'name', 'darwin'):
+        safe_print('Text')
     assert stdout.getvalue() == ''
     assert 'Text' in stderr.getvalue()
 
 
+@patch('yaml.safe_load')
 @patch('sys.stderr', new_callable=StringIO)
 @patch.object(safe_print, 'open', create=True)
-def test_safe_print_color(open: MagicMock, stderr: MagicMock):
-    safe_print('Text', color=colorama.Fore.RED)
+def test_safe_print_color(open: MagicMock, stderr: MagicMock, yaml_load: MagicMock):
+    yaml_load.return_value = {'colors': True}
+    with patch.object(os, 'name', 'nt'):
+        safe_print('Text', color=colorama.Fore.RED)
+    assert colorama.Fore.RED not in stderr.getvalue()
+    with patch.object(os, 'name', 'darwin'):
+        safe_print('Text', color=colorama.Fore.RED)
     assert colorama.Fore.RED in stderr.getvalue()
 
 
