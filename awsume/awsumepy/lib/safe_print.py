21ae69d59729f27cdfb48b89d5c9f5b0c14ed532
Michael Barney, Jr <mbarneyme@gmail.com>
2019-08-09 09:21:30 -0400

    refactors config management

--- awsume/awsumepy/lib/safe_print.py
+++ awsume/awsumepy/lib/safe_print.py
@@ -10,10 +10,9 @@ from . constants import AWSUME_CONFIG
 
 def safe_print(message: str, color: str = '', end: str = None):
     """Safely print so no data is interfering with the shell wrapper"""
-    try:
-        config = yaml.safe_load(open(str(AWSUME_CONFIG), 'r')) or {}
-    except:
-        config = {}
+    config = yaml.safe_load(open(str(AWSUME_CONFIG), 'r')) or {}
+    if not config:
+        config = {'colors': True}
     if os.name == 'nt' or config.get('colors') != True:
         color = ''
     print(str(color) + str(message) + colorama.Style.RESET_ALL, end=end, file=sys.stderr)
