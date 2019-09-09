00ab7f48fff7f824e7db41bd8fedf1623f904a42
Tom McLaughlin <tom@serverlessops.io>
2019-09-09 10:44:20 -0400

    Handle SAML 1 in addition to SAML 2.

--- awsume/awsumepy/lib/saml.py
+++ awsume/awsumepy/lib/saml.py
@@ -9,10 +9,18 @@ from . exceptions import SAMLAssertionParseError
 def parse_assertion(assertion: str) -> list:
     roles = []
     response = xmltodict.parse(base64.b64decode(assertion))
-    attributes = response.get('saml2p:Response', {}).get('saml2:Assertion', {}).get('saml2:AttributeStatement', {}).get('saml2:Attribute', {})
+
+    if response.get('saml2p:Response') is not None:
+        attributes = response.get('saml2p:Response', {}).get('saml2:Assertion', {}).get('saml2:AttributeStatement', {}).get('saml2:Attribute', {})
+        attribute_value_key = 'saml2:AttributeValue'
+    else:
+        attributes = response.get('samlp:Response', {}).get('saml:Assertion', {}).get('saml:AttributeStatement', {}).get('saml:Attribute', {})
+        attribute_value_key = 'saml:AttributeValue'
+
     if not attributes:
         raise SAMLAssertionParseError()
+
     for attribute in [_ for _ in attributes if _.get('@Name', '') == 'https://aws.amazon.com/SAML/Attributes/Role']:
-        for value in attribute['saml2:AttributeValue']:
+        for value in attribute[attribute_value_key]:
             roles.append(value['#text'])
     return roles
