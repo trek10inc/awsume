099ae1297f0ac5f0b2ead31605fdfd9116d696d0
Tom McLaughlin <tom@serverlessops.io>
2019-09-13 15:31:42 -0400

    Use parse_time() from lib.profile after it moved to there.

--- awsume/awsumepy/lib/aws.py
+++ awsume/awsumepy/lib/aws.py
@@ -125,5 +125,5 @@ def assume_role_with_saml(
     except Exception as e:
         raise RoleAuthenticationError(str(e))
     logger.debug('Role credentials received')
-    safe_print('Role credentials will expire {}'.format(parse_time(role_session['Expiration'])), colorama.Fore.GREEN)
+    safe_print('Role credentials will expire {}'.format(profile_lib.parse_time(role_session['Expiration'])), colorama.Fore.GREEN)
     return role_session
