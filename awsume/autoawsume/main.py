451f4cc0e6dfa12a04958fce5a3a0d09b71dbcf5
Michael Barney, Jr <mbarneyme@gmail.com>
2019-10-02 15:29:44 -0400

    fixes autoawsume again

--- awsume/autoawsume/main.py
+++ awsume/autoawsume/main.py
@@ -2,9 +2,12 @@ import json
 import subprocess
 import configparser
 import time
+import sys
 from datetime import datetime, timedelta
+import dateutil
 
 from ..awsumepy.lib.aws_files import get_aws_files, delete_section
+from ..awsumepy.lib import exceptions
 from .. import awsumepy
 
 
@@ -19,9 +22,6 @@ def main():
         for profile_name, auto_profile in auto_profiles.items():
             expiration = datetime.strptime(auto_profile['expiration'], '%Y-%m-%d %H:%M:%S')
             source_expiration = datetime.strptime(auto_profile['source_expiration'], '%Y-%m-%d %H:%M:%S')
-            #  + timedelta(minutes=5)
-            # awsumepy.awsume(*auto_profile.get('awsumepy_command').split(' '))
-            # expirations.append(datetime.now() + timedelta(hours=1))
             if source_expiration < datetime.now():
                 if expiration < datetime.now():
                     delete_section(profile_name, credentials_file)
@@ -29,8 +29,11 @@ def main():
                     expirations.append(expiration)
             else:
                 if expiration < datetime.now():
-                    session = awsumepy.awsume(*auto_profile.get('awsumepy_command').split(' '))
-                    expirations.append(session.awsume_credentials.get('Expiration'))
+                    session = refresh_profile(auto_profile)
+                    if session:
+                        expirations.append(session.awsume_credentials.get('Expiration'))
+                    else:
+                        delete_section(profile_name, credentials_file)
                 else:
                     expirations.append(expiration)
                     expirations.append(source_expiration)
@@ -38,6 +41,15 @@ def main():
         if not expirations:
             break
 
-        earliest_expiration = min(expirations)
+        local_expirations = [_.astimezone(dateutil.tz.tzlocal()) for _ in expirations]
+        earliest_expiration = min(local_expirations)
         time_to_sleep = (earliest_expiration - datetime.now().replace(tzinfo=earliest_expiration.tzinfo)).total_seconds() + 60
         time.sleep(time_to_sleep)
+
+
+def refresh_profile(auto_profile):
+    try:
+        session = awsumepy.awsume(*auto_profile.get('awsumepy_command').split(' '))
+        return session
+    except exceptions.AwsumeException:
+        return None
