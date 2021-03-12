0614daf97f05cc996c737fdabb1186b73b4b592d
Bob Crutchley <bob.crutchley@protonmail.com>
2021-03-12 14:41:33 +0000

    Added XDG Base Directory support.
    There is now support for the XDG base directories:
    - XDG_CONFIG_HOME
    - XDG_CACHE_HOME
    - XDG_DATA_HOME

--- awsume/awsumepy/lib/constants.py
+++ awsume/awsumepy/lib/constants.py
@@ -1,9 +1,22 @@
 from pathlib import Path
+import os
 
-AWSUME_DIR = Path('~/.awsume').expanduser()
-AWSUME_CONFIG = Path('~/.awsume' + '/config.yaml').expanduser()
+XDG_CONFIG_HOME = Path('~/.config').expanduser()
+if os.getenv('XDG_CONFIG_HOME'):
+    XDG_CONFIG_HOME = Path(os.getenv('XDG_CONFIG_HOME')).expanduser()
+
+XDG_DATA_HOME = Path('~/.local/share').expanduser()
+if os.getenv('XDG_DATA_HOME'):
+    XDG_DATA_HOME = Path(os.getenv('XDG_DATA_HOME')).expanduser()
+
+XDG_CACHE_HOME = Path('~/.cache').expanduser()
+if os.getenv('XDG_CACHE_HOME'):
+    XDG_CACHE_HOME = Path(os.getenv('XDG_CACHE_HOME')).expanduser()
+
+AWSUME_CONFIG = Path(str(XDG_CONFIG_HOME) + '/awsume/config.yaml')
+AWSUME_DIR = Path(str(XDG_DATA_HOME) + '/awsume')
+AWSUME_CACHE_DIR = Path(str(XDG_CACHE_HOME) + '/awsume')
 
 DEFAULT_CREDENTIALS_FILE = Path('~/.aws/credentials').expanduser()
 DEFAULT_CONFIG_FILE = Path('~/.aws/config').expanduser()
 
-AWSUME_CACHE_DIR = Path('~/.awsume/cache').expanduser()
