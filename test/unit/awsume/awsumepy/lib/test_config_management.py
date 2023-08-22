e9aba8dec03367961961c53b0253effc922eb772
Matt Skillman <mskillman@trek10.com>
2023-08-22 14:29:18 -0800

    #132 adjust tests

--- test/unit/awsume/awsumepy/lib/test_config_management.py
+++ test/unit/awsume/awsumepy/lib/test_config_management.py
@@ -7,12 +7,19 @@ import yaml
 from awsume.awsumepy.lib import config_management, exceptions
 
 
+@patch('awsume.awsumepy.lib.constants.IS_USING_XDG_CONFIG_HOME', False)
 @patch('yaml.safe_load')
 @patch('builtins.open')
 @patch('os.makedirs')
 @patch('os.path.isfile')
 @patch('os.path.exists')
-def test_load_config(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock, yaml_load: MagicMock):
+def test_load_config(
+        exists: MagicMock,
+        isfile: MagicMock,
+        makedirs: MagicMock,
+        open: MagicMock,
+        yaml_load: MagicMock,
+):
     exists.return_value = True
     isfile.return_value = True
     yaml_load.return_value = {'key': 'value'}
@@ -22,13 +29,14 @@ def test_load_config(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, 
     assert result == yaml_load.return_value
     makedirs.assert_not_called()
 
-
+@patch('awsume.awsumepy.lib.constants.IS_USING_XDG_CONFIG_HOME', False)
 @patch('yaml.safe_load')
 @patch('builtins.open')
 @patch('os.makedirs')
 @patch('os.path.isfile')
 @patch('os.path.exists')
-def test_load_config_no_path(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock, yaml_load: MagicMock):
+def test_load_config_no_path(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock,
+                             yaml_load: MagicMock):
     exists.return_value = False
     isfile.return_value = True
     yaml_load.return_value = {'key': 'value'}
@@ -40,17 +48,17 @@ def test_load_config_no_path(exists: MagicMock, isfile: MagicMock, makedirs: Mag
     open.assert_called_once()
     assert makedirs.call_count == 2
 
-
+@patch('awsume.awsumepy.lib.constants.IS_USING_XDG_CONFIG_HOME', False)
 @patch('yaml.safe_load')
 @patch('builtins.open')
 @patch('os.makedirs')
 @patch('os.path.isfile')
 @patch('os.path.exists')
-def test_load_config_no_file(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock, yaml_load: MagicMock):
+def test_load_config_no_file(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock,
+                             yaml_load: MagicMock):
     exists.return_value = True
     isfile.return_value = False
     yaml_load.return_value = {'key': 'value'}
-
 
     result = config_management.load_config()
 
@@ -58,7 +66,7 @@ def test_load_config_no_file(exists: MagicMock, isfile: MagicMock, makedirs: Mag
     makedirs.assert_not_called()
     assert open.call_count == 2
 
-
+@patch('awsume.awsumepy.lib.constants.IS_USING_XDG_CONFIG_HOME', False)
 @patch.object(config_management, 'safe_print')
 @patch.object(config_management, 'write_config')
 @patch('yaml.safe_load')
@@ -66,7 +74,8 @@ def test_load_config_no_file(exists: MagicMock, isfile: MagicMock, makedirs: Mag
 @patch('os.makedirs')
 @patch('os.path.isfile')
 @patch('os.path.exists')
-def test_load_config_yaml_error(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock, yaml_load: MagicMock, write_config: MagicMock, safe_print: MagicMock):
+def test_load_config_yaml_error(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock,
+                                yaml_load: MagicMock, write_config: MagicMock, safe_print: MagicMock):
     exists.return_value = True
     isfile.return_value = True
     yaml_load.side_effect = yaml.error.YAMLError()
@@ -77,14 +86,14 @@ def test_load_config_yaml_error(exists: MagicMock, isfile: MagicMock, makedirs: 
     makedirs.assert_not_called()
     open.assert_called_once()
 
-
 @patch.object(config_management, 'safe_print')
 @patch('yaml.safe_dump')
 @patch('builtins.open')
 @patch('os.makedirs')
 @patch('os.path.isfile')
 @patch('os.path.exists')
-def test_write_config(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock, yaml_dump: MagicMock, safe_print: MagicMock):
+def test_write_config(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock, yaml_dump: MagicMock,
+                      safe_print: MagicMock):
     exists.return_value = True
     isfile.return_value = True
 
@@ -101,7 +110,8 @@ def test_write_config(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock,
 @patch('os.makedirs')
 @patch('os.path.isfile')
 @patch('os.path.exists')
-def test_write_config_no_path(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock, yaml_dump: MagicMock, safe_print: MagicMock):
+def test_write_config_no_path(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock,
+                              yaml_dump: MagicMock, safe_print: MagicMock):
     exists.return_value = False
     isfile.return_value = True
 
@@ -118,7 +128,8 @@ def test_write_config_no_path(exists: MagicMock, isfile: MagicMock, makedirs: Ma
 @patch('os.makedirs')
 @patch('os.path.isfile')
 @patch('os.path.exists')
-def test_write_config_no_file(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock, yaml_dump: MagicMock, safe_print: MagicMock):
+def test_write_config_no_file(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock,
+                              yaml_dump: MagicMock, safe_print: MagicMock):
     exists.return_value = True
     isfile.return_value = False
 
@@ -135,7 +146,8 @@ def test_write_config_no_file(exists: MagicMock, isfile: MagicMock, makedirs: Ma
 @patch('os.makedirs')
 @patch('os.path.isfile')
 @patch('os.path.exists')
-def test_write_config_catches_exception(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock, yaml_dump: MagicMock, safe_print: MagicMock):
+def test_write_config_catches_exception(exists: MagicMock, isfile: MagicMock, makedirs: MagicMock, open: MagicMock,
+                                        yaml_dump: MagicMock, safe_print: MagicMock):
     exists.return_value = True
     isfile.return_value = False
     yaml_dump.side_effect = Exception()
