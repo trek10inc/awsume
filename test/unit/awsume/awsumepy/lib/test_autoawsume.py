9290efe16b49218b7e91c8ff6c874fffe5a656ee
Matt Skillman <mskillman@trek10.com>
2023-09-12 13:58:12 -0800

    #172 adjust implementation + add test case to cover this scenario

--- test/unit/awsume/awsumepy/lib/test_autoawsume.py
+++ test/unit/awsume/awsumepy/lib/test_autoawsume.py
@@ -31,3 +31,28 @@ def test_create_autoawsume_profile(profile: MagicMock, aws_files: MagicMock):
         'source_expiration': now_str,
         'awsumepy_command': 'awsumepy profile',
     }, 'credentials/file', True)
+
+
+@patch.object(autoawsume, 'aws_files_lib')
+@patch.object(autoawsume, 'profile_lib')
+def test_create_autoawsume_profile_no_expiration(profile: MagicMock, aws_files: MagicMock):
+    aws_files.get_aws_files.return_value = 'config/file', 'credentials/file'
+    profile.credentials_to_profile.return_value = {
+        'aws_access_key_id': 'AKIA...',
+        'aws_secret_access_key': 'SECRET',
+        'aws_session_token': 'LONG',
+    }
+    config = {}
+    profiles = {}
+    role_session = {}
+    args = argparse.Namespace(target_profile_name='profile', system_arguments=['awsumepy', 'profile'], output_profile=None)
+
+    autoawsume.create_autoawsume_profile(config, args, profiles, role_session)
+
+    aws_files.add_section.assert_called_with('autoawsume-profile', {
+        'aws_access_key_id': 'AKIA...',
+        'aws_secret_access_key': 'SECRET',
+        'aws_session_token': 'LONG',
+        'autoawsume': 'true',
+        'awsumepy_command': 'awsumepy profile',
+    }, 'credentials/file', True)
